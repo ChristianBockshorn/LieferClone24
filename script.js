a87c2b43db8baea1764481fe051f1c074ca4c42a
Christian Bockshorn <bockshorn@gmx.net>
2023-05-16 08:53:31 +0200

    header-dropdown

---
+++ script.js
@@ -0,0 +1,7 @@
+function openHeaderMenuMore() {
+    openHeaderMenuMoreBackground.style.display = 'none';
+}
+
+function closeHeaderMenuMore() {
+    openHeaderMenuMoreBackground.style.display = 'flex';
+}
