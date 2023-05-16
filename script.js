ffec1848b8a6a2f69c492392f3298120b4f1b239
Christian Bockshorn <bockshorn@gmx.net>
2023-05-16 19:28:04 +0200

    header-popupWindow

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// function openHeaderMenuMore() {
+//     openHeaderMenuMoreBackground.style.display = 'flex';
+// }
+
+// function closeHeaderMenuMore() {
+//     openHeaderMenuMoreBackground.style.display = 'none';
+// }
+
+
 function openHeaderMenuMore() {
-    openHeaderMenuMoreBackground.style.display = 'none';
+    document.getElementById('openHeaderMenuMoreBackground').classList.remove('d-none');
+    
 }
 
-function closeHeaderMenuMore() {
-    openHeaderMenuMoreBackground.style.display = 'flex';
+function closeHeaderMenuMore(){
+    document.getElementById('openHeaderMenuMoreBackground').classList.add('d-none');
+    
+}
+
+function doNotClose(event) {
+    event.stopPropagation();    
 }
