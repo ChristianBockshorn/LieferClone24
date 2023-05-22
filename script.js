c86c960186f43e53d21cf0b3d926c9cd3b5d517c
Christian Bockshorn <bockshorn@gmx.net>
2023-05-22 13:59:53 +0200

    scrollbar-2

--- script.js
+++ script.js
@@ -9,14 +9,24 @@
 
 function openHeaderMenuMore() {
     document.getElementById('openHeaderMenuMoreBackground').classList.remove('d-none');
-    
+
 }
 
-function closeHeaderMenuMore(){
+function closeHeaderMenuMore() {
     document.getElementById('openHeaderMenuMoreBackground').classList.add('d-none');
-    
+
 }
 
 function doNotClose(event) {
-    event.stopPropagation();    
+    event.stopPropagation();
+}
+
+function scrollArrow() {
+    const box = document.getElementById("box");
+    box.scrollTo({
+        
+        left: 450,
+        behavior: 'smooth'
+    });
+
 }
