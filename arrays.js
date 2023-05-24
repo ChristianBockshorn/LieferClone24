8a1a624863e2ae2e0e673453d77f47a6ca4e64c5
Christian Bockshorn <bockshorn@gmx.net>
2023-05-24 18:16:20 +0200

    product-card

---
+++ arrays.js
@@ -0,0 +1,35 @@
+let dishes = [
+    {
+        'category': 'Beliebt',
+        'menuName': 'Pizza Margherita',
+        'image': 'img/pizza/PizzaMargherita.jpg',
+        'description': 'mit Tomatensauce, Käse',
+        'price': '7,90 €',
+
+    },
+
+    {
+        'category': '',
+        'menuName': 'Pizza Salami',
+        'image': 'img/pizza/PizzaSalami.jpg',
+        'description': 'mit Tomatensauce, Käse',
+        'price': '8,90 €',
+    },
+
+    {
+        'category': '',
+        'menuName': 'Pizza Margherita',
+        'image': 'img/pizza/PizzaMargherita.jpg',
+        'description': 'mit Tomatensauce, Käse',
+        'price': '7,90 €',
+
+    },
+
+    {
+        'category': '',
+        'menuName': 'Pizza Salami',
+        'image': 'img/pizza/PizzaSalami.jpg',
+        'description': 'mit Tomatensauce, Käse',
+        'price': '8,90 €',
+    },
+]
--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+function render() {
+    let content = document.getElementById('content'); //div container wird verknüpft mit der id 'content' d.h: wir haben eine variable wo unser div-container drin ist
+    content.innerHTML = '';
+    /*div-container wird gelöscht.
+    sonst werden die Elemente einfach immer weiter hinzugefügt werden. 
+    Wenn man es aber löscht und dann hinzufügt, hast du immer nur die Elemente die du brauchst und diese "up to date". 
+    Im Grunde genommen, löscht man den Content und zeichnet diesen dann neu und das mit neuen "Daten".
+    -->Der div container wird an den Inhalt angepasst falls sich dieser ändert
+    */
+
+
+    for (let i = 0; i < dishes.length; i++) {
+        const dish = dishes[i];
+
+        content.innerHTML += `
+        <h2>${dish['category']}</h2>
+            <div class="card">
+                <h2>${dish['menuName']}</h2>
+                
+                <div class="container">
+                    <img class="image" src="${dish.image}">
+                    
+                    <div class="middle">
+                        <div class="text">Hinzufügen</div>
+                    </div>
+                </div>
+                
+                
+
+                <h3>${dish['description']}</h3>
+                <h2>${dish['price']}</h2>
+
+       
+            </div>
+        `;
+    }
+}
+
+
+
+
+
+
 // function openHeaderMenuMore() {
 //     openHeaderMenuMoreBackground.style.display = 'flex';
 // }
@@ -24,7 +67,7 @@ function doNotClose(event) {
 function scrollArrow() {
     const box = document.getElementById("box");
     box.scrollTo({
-        
+
         left: 450,
         behavior: 'smooth'
     });
