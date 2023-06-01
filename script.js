a72870b645cd2cb358ff72857b914bde7ca1b00c
Christian Bockshorn <bockshorn@gmx.net>
2023-06-01 18:06:10 +0200

    basket-JS

--- script.js
+++ script.js
@@ -10,27 +10,20 @@ let basketPrices = [];
 function render() {
     let content = document.getElementById('content'); //div container wird verknüpft mit der id 'content' d.h: wir haben eine variable wo unser div-container drin ist
     content.innerHTML = '';
-    /*div-container wird gelöscht.
-    sonst werden die Elemente einfach immer weiter hinzugefügt werden. 
-    Wenn man es aber löscht und dann hinzufügt, hast du immer nur die Elemente die du brauchst und diese "up to date". 
-    Im Grunde genommen, löscht man den Content und zeichnet diesen dann neu und das mit neuen "Daten".
-    -->Der div container wird an den Inhalt angepasst falls sich dieser ändert
-    */
-
 
     for (let i = 0; i < dishes.length; i++) {
         let dish = dishes[i];
 
         content.innerHTML += `
         <h2>${dish['category']}</h2>
-            <div class="card">
+            <div id="card(${i})" class="card">
                 <h2>${dish['menuName']}</h2>
                 
                 <div class="container">
                     <img class="image" src="${dish.image}">
                     
                     <div class="middle">
-                        <div><button onclick="renderBasketCard()" class="add-product-button">Hinzufügen</button></div>
+                        <div><button onclick="addMenu(${i})" class="add-product-button">Hinzufügen</button></div>
                     </div>
                 </div>
                 
@@ -39,20 +32,34 @@ function render() {
             </div>
         `;
     }
-    
+
+}
+
+
+function addMenu(menuName, price) {
+    let index = dishes.indexOf(menuName);
+    let food = dishes.menuName
+
+        
+
+    render();
+    renderBasketCard();
 }
 
 function renderBasketCard() {
     let fullShoppingCard = document.getElementById('fullShoppingCard');
     fullShoppingCard = '';
 
-    
+
 
     for (let b = 0; b < basketCardTemplate.length; b++) {
+
+        let item = dishes.menuName;
+
+
+        fullShoppingCard.innerHTML += `
         
-        fullShoppingCard.innerHTML +=`
-        test
-        <div id="fullShoppingCard" class="d-none">
+        <div id="fullShoppingCard">
             <div id="orderContainer">
                 <div class="order-menu">
                     <div class="order-menu-name">
@@ -67,15 +74,10 @@ function renderBasketCard() {
                 <div class="order-add-reduce">
 
                 </div>
-
-
-
-
             </div>
-
         </div>
         `;
-        
+
     }
     fullBasket();
 }
